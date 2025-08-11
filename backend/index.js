16a1e6e7b12497aaa3b9a2e6d75e73be447b87da
Pawketz <jedyer1@asu.edu>
2025-08-11 12:40:39 -0400

    modified index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 require('dotenv').config()
 
-const app = require('./app')
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+
+app.get('/api/health', (req, res) => {
+  res.send({ status: 'ok' })
+})
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
